fc917c2310024be287c31abf88904687899f65a0
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-05-30 12:15:40 +0545

    assignment 2 completed

--- JS/Js p1/assignment.js
+++ JS/Js p1/assignment.js
@@ -8,3 +8,6 @@ else{
 // Q2--take the user's name and age aas input using prompt.Then return back the follwoing statement to the user as an alert (by substituting their name and age)
 let name= prompt("Enter your name");
 let age = prompt("Enter your age");
+alert(`$name is $age years old`);
+
+let number 
