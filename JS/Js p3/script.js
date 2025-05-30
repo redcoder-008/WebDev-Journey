ca0735f2c512a5177ac2baeaa04d903e51c3f671
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-05-30 14:04:20 +0545

    indexof

--- JS/Js p3/script.js
+++ JS/Js p3/script.js
@@ -1,6 +1,9 @@
 // let str = prompt("Apka naam");
 let str =" kaRan";
-console.log(str.trim());
+// console.log(str.trim());
 console.log("hw")
 console.log(str.toUpperCase());
 console.log(str.toLowerCase());
+//index of 
+console.log(str.indexOf('a'));
+//method chaining
