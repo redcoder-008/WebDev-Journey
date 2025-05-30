ba1e4619c9a5eeb42d3ce720fb9aa65074dff402
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-05-30 12:13:50 +0545

    assignment question 1

---
+++ JS/Js p1/assignment.js
@@ -0,0 +1,10 @@
+let num =10;
+if(num%2==0){
+    console.log("good");
+}
+else{
+    console.log("bad");
+}
+// Q2--take the user's name and age aas input using prompt.Then return back the follwoing statement to the user as an alert (by substituting their name and age)
+let name= prompt("Enter your name");
+let age = prompt("Enter your age");
