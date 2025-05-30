7c48690e5d439f1624b27330c3b61c50a6db5a66
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-05-30 14:00:05 +0545

    upper case and lower case

--- JS/Js p3/script.js
+++ JS/Js p3/script.js
@@ -1,2 +1,6 @@
-let str = prompt("Apka naam");
+// let str = prompt("Apka naam");
+let str =" kaRan";
 console.log(str.trim());
+console.log("hw")
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
