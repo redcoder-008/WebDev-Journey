efd2c31fbbe219d2f4bac6333733eaf6026652ca
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-05-30 14:07:23 +0545

    method chaining

--- JS/Js p3/script.js
+++ JS/Js p3/script.js
@@ -1,5 +1,5 @@
 // let str = prompt("Apka naam");
-let str =" kaRan";
+let str ="          kaRan";
 // console.log(str.trim());
 console.log("hw")
 console.log(str.toUpperCase());
@@ -7,3 +7,5 @@ console.log(str.toLowerCase());
 //index of 
 console.log(str.indexOf('a'));
 //method chaining
+let newstr= str.toUpperCase().trim();
+console.log(newstr);
