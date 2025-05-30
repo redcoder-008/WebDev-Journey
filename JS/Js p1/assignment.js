b47fe699020a805d7e7c9531fc1c182c140347e9
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-05-30 12:19:20 +0545

    question 3 completed

--- JS/Js p1/assignment.js
+++ JS/Js p1/assignment.js
@@ -6,8 +6,25 @@ else{
     console.log("bad");
 }
 // Q2--take the user's name and age aas input using prompt.Then return back the follwoing statement to the user as an alert (by substituting their name and age)
-let name= prompt("Enter your name");
-let age = prompt("Enter your age");
-alert(`$name is $age years old`);
+// let name= prompt("Enter your name");
+// let age = prompt("Enter your age");
+// alert(`$name is $age years old`);
 
-let number 
+let number = 3;
+switch (number){
+case 1 :
+    console.log("January,February,March");
+    break;
+    case 2:
+        console.log("April,May,June");
+        break;
+    case 3:
+        console.log("July, August, September");
+        break;
+    case 4:
+        console.log("October,November,December");
+        break;
+    default:
+        console.error("Wrong input;")
+}
+     
