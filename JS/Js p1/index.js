231ec5586342356aa4a4d0b91097934e50f6b74b
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-05-30 09:34:20 +0545

    Console .log

--- JS/Js p1/index.js
+++ JS/Js p1/index.js
@@ -4,3 +4,7 @@ let firstname= 'Karan';
 console.log(firstname[0]);
 
 console.log(firstname[firstname.length-1]);
+let pen=10;
+let pencil=5;
+let output = `The total price is : ${pen+pencil} Rupees`;
+console.log(output);
