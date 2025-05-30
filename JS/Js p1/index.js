c6d9c672461e98419c960471d39cb209b2816c6e
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-05-30 10:17:31 +0545

    Switch Statement

--- JS/Js p1/index.js
+++ JS/Js p1/index.js
@@ -51,3 +51,30 @@ if(string[0]=='a' && string.length > 3 ){
 }else{
     console.log('it is not a good string')
 }
+//switch statement to print number of days
+let num =5;
+switch (num){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("saturday");
+        break;
+    case 7:
+        console.log("sunday");
+        break;
+        default :
+        console.log("Wrong Input");
+}
