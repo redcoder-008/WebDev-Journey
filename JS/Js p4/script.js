0b8f551c5870d562f8aac492a6cf85929f8938a9
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-05-31 17:50:48 +0545

    about loops in js

--- JS/Js p4/script.js
+++ JS/Js p4/script.js
@@ -1,41 +1,41 @@
-// console.log("hello world");
-// for(let i=1;i<=10;i++){
-//     if(i%2==1) continue;
-//     console.log(i);
+console.log("hello world");
+for(let i=1;i<=10;i++){
+    if(i%2==1) continue;
+    console.log(i);
 
-// }
-// for(let i=1;i<10;i++){
-//     for(let j=1;j<=i;j++){
-//         console.log(j);
-//     }
-// }
+}
+for(let i=1;i<10;i++){
+    for(let j=1;j<=i;j++){
+        console.log(j);
+    }
+}
 
 //favourite movie
 
-// const favmov="avatar";
-// // let guess = prompt("Enter the movie name");
-// while((guess != favmov) && (guess != "quit")){
+const favmov="avatar";
+// let guess = prompt("Enter the movie name");
+while((guess != favmov) && (guess != "quit")){
     
-// //     guess = prompt("Wrong!! Please Try Again");
+//     guess = prompt("Wrong!! Please Try Again");
 
-// }
+}
 
-// if(guess == 'quit'){
-//     console.log("Looser")
-// }
+if(guess == 'quit'){
+    console.log("Looser")
+}
 
-// if(favmov==guess){
-//     console.log("congratulation");
-// }
+if(favmov==guess){
+    console.log("congratulation");
+}
 
 //Nested loops in array
 let fruits =[['mango','banana'],['orange','litchee'],['apple','grapes']];
-// for(let i=0;i<fruits.length;i++){
-//     console.log(fruits[i]);
-//     for(let j=0;j<fruits[i].length;j++){
-//         console.log(`j=${j} , ${fruits[i][j]}`);
-//     }
-// }
+for(let i=0;i<fruits.length;i++){
+    console.log(fruits[i]);
+    for(let j=0;j<fruits[i].length;j++){
+        console.log(`j=${j} , ${fruits[i][j]}`);
+    }
+}
 
 //for of loop
 for(fruit of fruits){
