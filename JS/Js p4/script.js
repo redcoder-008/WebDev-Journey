664f2d7ba8b567daa35c48dfd028782899391fe0
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-05-31 17:50:00 +0545

    Nested for of loop

--- JS/Js p4/script.js
+++ JS/Js p4/script.js
@@ -30,10 +30,23 @@
 
 //Nested loops in array
 let fruits =[['mango','banana'],['orange','litchee'],['apple','grapes']];
-for(let i=0;i<fruits.length;i++){
-    console.log(fruits[i]);
-    for(let j=0;j<fruits[i].length;j++){
-        console.log(`j=${j} , ${fruits[i][j]}`);
+// for(let i=0;i<fruits.length;i++){
+//     console.log(fruits[i]);
+//     for(let j=0;j<fruits[i].length;j++){
+//         console.log(`j=${j} , ${fruits[i][j]}`);
+//     }
+// }
+
+//for of loop
+for(fruit of fruits){
+    console.log(fruit);
+}
+for(char of 'karan'){
+    console.log(char);
+}
+//nested for of loop
+for(fruit of fruits){
+    for(internal of fruit){
+        console.log(internal);
     }
 }
-
