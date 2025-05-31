12c6327ff6cd2a6324f72ebc707d9e3a0318f239
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-05-31 17:26:56 +0545

    guessing movie app

--- JS/Js p4/script.js
+++ JS/Js p4/script.js
@@ -1,11 +1,28 @@
-console.log("hello world");
-for(let i=1;i<=10;i++){
-    if(i%2==1) continue;
-    console.log(i);
+// console.log("hello world");
+// for(let i=1;i<=10;i++){
+//     if(i%2==1) continue;
+//     console.log(i);
+
+// }
+// for(let i=1;i<10;i++){
+//     for(let j=1;j<=i;j++){
+//         console.log(j);
+//     }
+// }
+//favourite movie
+
+const favmov="avatar";
+let guess = prompt("Enter the movie name");
+while((guess != favmov) && (guess != "quit")){
+    
+    guess = prompt("Wrong!! Please Try Again");
 
 }
-for(let i=1;i<10;i++){
-    for(let j=1;j<=i;j++){
-        console.log(j);
-    }
+
+if(guess == 'quit'){
+    console.log("Looser")
+}
+
+if(favmov==guess){
+    console.log("congratulation");
 }
