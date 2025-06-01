611cde526143085dabda3a183ab8703227f0aa0c
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-01 16:56:20 +0545

    sum of digits in a nymber

--- JS/Js p4/Assignment/script.js
+++ JS/Js p4/Assignment/script.js
@@ -7,4 +7,26 @@ for(let i =0;i<arr.length;i++){
     }
 }
 
-//Qs2 
+//Qs2 wap to fond the no of digits in a numbers;
+let number =131213;
+let count =0;
+ while(number>0){
+    // number/=10; 
+    number = Math.floor(number/10);
+
+    count++;
+}
+console.log( `Total no of digits in  ${num} is ${count}`);
+
+//Q3 find the sum of digits
+ let numb= 287152;
+ let sum=0;
+ let copy = numb;
+ while(copy >0){
+    //  let remainder= Math.floor(copy%10);
+     let remainder=(copy%10);
+     console.log(remainder);
+   copy=Math.floor(copy/10);
+   sum+=remainder;
+}
+console.log(sum)
