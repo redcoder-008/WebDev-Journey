ab5431cee16b95832ffa79240d9120eb07200cd3
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-01 18:09:51 +0545

    get/update value in obj of js

---
+++ JS/Js p5/script.js
@@ -0,0 +1,16 @@
+const student ={
+    name:"karan",
+    age : 20,
+    home: "germany",
+    tech :["Java","DSA"]
+
+};
+const post={
+    username:"karankewat008",
+    content : "coding vlog",
+    likes :100000,
+    comment : 40000,
+    shares:1000,
+    repost:100,
+    tags:["karan","elon musk","Tim cook"]
+}
