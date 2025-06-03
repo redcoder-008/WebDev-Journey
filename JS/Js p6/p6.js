d4550e4012ca74091d5ffcec87b0241c6012d7d5
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-03 20:03:50 +0545

    roll a dice using finction

--- JS/Js p6/p6.js
+++ JS/Js p6/p6.js
@@ -1,3 +1,13 @@
 function print(){
     console.log("Hello world ")
 }
+
+// PQ 1 write a function to print a poem
+function poem(){
+    console.log("Twinkle twinkle little star");
+}
+ function roll(){
+    let random = Math.floor((Math.random()*6) +1);
+    console.log(random)
+ }
+ roll();
