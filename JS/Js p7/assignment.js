de9c4f8b2641feb162ce4984de4770ee25e12a71
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-04 15:07:44 +0545

    assignment 7

--- JS/Js p7/assignment.js
+++ JS/Js p7/assignment.js
@@ -24,3 +24,18 @@ let out= average(arr);
 // Q2) 
 
 let check = n => n%2==0;
+
+let length = 4;
+
+function callback() {
+  console.log(this.length);
+}
+
+const object = {
+  length: 5,
+  method(callback) {
+    callback(); // call the function
+  },
+};
+
+object.method(callback, 1, 2);
