bd874c5146aab675adc0176ece9aac9d62f09631
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-04 14:51:58 +0545

    setInterval & setTimeout function in js

--- JS/Js p7/script.js
+++ JS/Js p7/script.js
@@ -24,16 +24,31 @@ catch(exp){
 
 
 // set timme out
+// setTimeout(()=>{
+//     console.log("Hello")
+// },3000);
+// console.log("timer")
+//  // set interval
+
+//  let id = setInterval(()=>{
+//     console.log("Hellow");
+//  },1000);
+
+//  console.log( "id",id);
+// write an arrow function that returns the squate of a numner n
+
+ const square = n =>{
+    return n*n;
+}
+
+
+// ..write a function that prints "hello world " 5 times at the interval of 2s each
+
+let idx = setInterval(()=>{
+    console.log("Hellow world");
+},2000);
+
 setTimeout(()=>{
-    console.log("Hello")
-},3000);
-console.log("timer")
- // set interval
-
- let id = setInterval(()=>{
-    console.log("Hellow");
- },1000);
-
- console.log( "id",id);
-// 
+    clearInterval(idx)
+},10000);
 
