9f3418708de9b6a89e690c0dbc1a84514a84de8e
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-04 14:07:22 +0545

    This keyword

---
+++ JS/Js p7/script.js
@@ -0,0 +1,15 @@
+//this keyword
+const student ={
+    name:"Karan",
+    java:60,
+    dsa:40,
+    javascript:60,
+
+  average(){
+        return  (this.java+this.dsa+this.javascript)/3;
+
+    }
+
+
+
+}
