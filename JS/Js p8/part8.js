495fd41b20ac23bdad7cce7083e065fe9d2fb115
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-05 18:02:04 +0545

    filter

--- JS/Js p8/part8.js
+++ JS/Js p8/part8.js
@@ -1,4 +1,4 @@
-let arr=[1,2,3,4,5];
+let arr=[1,2,3,4,5,6];
 function print(item){
     console.log(item);
 }
@@ -8,3 +8,24 @@ arr.forEach( (item)=> {console.log(item)});
 let square = arr.map(el =>{
     return el*el;
 })
+
+let student = [
+    {
+        name:"karan",
+        mark:98,
+    },
+    {
+        name:"kamat",
+        mark:88,
+    },{
+        name:"kumar",
+        mark:99,
+    },
+]
+let gpa = student.map((el) => {
+    return el.mark/10;
+        
+    });
+    let ans = arr.filter((el)=>{
+        return el%2==0;
+    })
