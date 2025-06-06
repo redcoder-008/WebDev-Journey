c4eebfff1e3e5d78d0fde6ca7e9e865a1df373f9
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-06 21:19:59 +0545

    manipulating style

--- JS/Js p9/app.js
+++ JS/Js p9/app.js
@@ -1,2 +1,18 @@
-document[8].innerText = "karan";
+// document[8].innerText = "karan";
  document.getElementById("mainImg"); //get element by id
+ document.getElementsByClassName("kk");//get element by class name
+
+ let img = document.querySelector('img');
+
+ let attr= img.getAttribute(img);
+ img.setAttribute('src','assets/creation_1.png')
+
+ let heading= document.querySelector("h1");
+ heading.style.color='red'; 
+
+ let links = document.querySelectorAll("a");
+//  links.style.color='red';
+ for(i of links){
+    i.style.color= "red";
+
+ }
