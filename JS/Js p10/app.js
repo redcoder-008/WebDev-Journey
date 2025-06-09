a52282d25e51c3577b7e7aded4e9083eb35e3202
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-09 22:14:42 +0545

    keyboaard event

--- JS/Js p10/app.js
+++ JS/Js p10/app.js
@@ -1,59 +1,79 @@
-//  let btn = document.querySelectorAll('button');
-//  console.dir(btn);
-//  btn.onclick = function() {
-//     console.warn("Button was clicked");
-//  }
-
-// //  btn.onclick= sayhello
-//  function sayhello(){
-//     console.log("hello");
-//  }
-//  function doubleclick(){
-//     console.log("doubleclicked");
-//  }
-// //  for(item of btn){
-// //     item.onmouseenter= sayhello;
+// //  let btn = document.querySelectorAll('button');
+// //  console.dir(btn);
+// //  btn.onclick = function() {
+// //     console.warn("Button was clicked");
 // //  }
 
-//  for(item of btn){
-//     item.addEventListener("click",sayhello) ;
-//     item.addEventListener("dblclick",doubleclick) ;
+// // //  btn.onclick= sayhello
+// //  function sayhello(){
+// //     console.log("hello");
+// //  }
+// //  function doubleclick(){
+// //     console.log("doubleclicked");
+// //  }
+// // //  for(item of btn){
+// // //     item.onmouseenter= sayhello;
+// // //  }
+
+// //  for(item of btn){
+// //     item.addEventListener("click",sayhello) ;
+// //     item.addEventListener("dblclick",doubleclick) ;
+// //  }
+// let btnn = document.querySelector("button");
+// btnn.onclick=sayhi;
+
+// function sayhi(){
+//     console.log("random color was generated ");
+// }
+
+// function getrandomcolor(){
+//     let red = Math.floor(Math.random()*255);
+//     let green = Math.floor(Math.random()*255);
+//     let blue = Math.floor(Math.random()*255);
+//     let color = `rgb(${red}, ${green} , ${blue})`;
+//     return color;
+// }
+
+// btnn.addEventListener("click",function(){
+// let h2= document.querySelector('h2');
+// // h2.innerText = getrandomcolor();
+// h2.style.color = getrandomcolor();
+// document.querySelector('div').style.backgroundColor= getrandomcolor();
+
+// })
+
+// let box = document.querySelector('div');
+// box.addEventListener("click" , function(){
+//     console.log("box is clicked");
+// })
+
+//  function changeColor(){
+//      console.dir(this.innerText);
+//     this.style.backgroundColor = "aqua";
 //  }
-let btnn = document.querySelector("button");
-btnn.onclick=sayhi;
 
-function sayhi(){
-    console.log("random color was generated ");
-}
+//  box.addEventListener("click" ,changeColor);
+//  btnn.addEventListener("click" ,changeColor);
+//  box.addEventListener("click" ,changeColor);
+//  box.addEventListener("click" ,changeColor);
 
-function getrandomcolor(){
-    let red = Math.floor(Math.random()*255);
-    let green = Math.floor(Math.random()*255);
-    let blue = Math.floor(Math.random()*255);
-    let color = `rgb(${red}, ${green} , ${blue})`;
-    return color;
-}
 
-btnn.addEventListener("click",function(){
-let h2= document.querySelector('h2');
-// h2.innerText = getrandomcolor();
-h2.style.color = getrandomcolor();
-document.querySelector('div').style.backgroundColor= getrandomcolor();
+let inp =document.querySelector("input");
 
+inp.addEventListener("keydown",function(event){
+    console.log("key = " ,event.key);
+    console.log("Code = " ,event.code);
+    console.warn("key was pressed");
+    if(event.key == "ArrowUp"){
+        console.log("upar jaa rha hai");
+    } else if(event.key == "ArrowDown"){
+        console.log("niche jaa rha hai")
+
+    }else if(event.key == "ArrowLeft"){
+        console.log("left jaa rha hai");
+
+    }
+    else if(event.key == "ArrowRight"){
+        console.log("Right jaa rha hai");
+    }
 })
-
-let box = document.querySelector('div');
-box.addEventListener("click" , function(){
-    console.log("box is clicked");
-})
-
- function changeColor(){
-     console.dir(this.innerText);
-    this.style.backgroundColor = "aqua";
- }
-
- box.addEventListener("click" ,changeColor);
- btnn.addEventListener("click" ,changeColor);
- box.addEventListener("click" ,changeColor);
- box.addEventListener("click" ,changeColor);
-
