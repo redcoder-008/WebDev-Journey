ec29a5b77fcee405de6ae0aa2692823014f63488
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-09 21:31:00 +0545

    Random color on a click

--- JS/Js p10/app.js
+++ JS/Js p10/app.js
@@ -1,18 +1,44 @@
- let btn = document.querySelectorAll('button');
- console.dir(btn);
- btn.onclick = function() {
-    console.warn("Button was clicked");
- }
-
- btn.onclick= sayhello
- function sayhello(){
-    console.log("hello");
- }
-//  for(item of btn){
-//     item.onmouseenter= sayhello;
+//  let btn = document.querySelectorAll('button');
+//  console.dir(btn);
+//  btn.onclick = function() {
+//     console.warn("Button was clicked");
 //  }
 
- for(item of btn){
-    item.addEventListener("click",sayhello) ;
- }
+// //  btn.onclick= sayhello
+//  function sayhello(){
+//     console.log("hello");
+//  }
+//  function doubleclick(){
+//     console.log("doubleclicked");
+//  }
+// //  for(item of btn){
+// //     item.onmouseenter= sayhello;
+// //  }
+
+//  for(item of btn){
+//     item.addEventListener("click",sayhello) ;
+//     item.addEventListener("dblclick",doubleclick) ;
+//  }
+let btnn = document.querySelector("button");
+btnn.onclick=sayhi;
+
+function sayhi(){
+    console.log("random color was generated ");
+}
+
+function getrandomcolor(){
+    let red = Math.floor(Math.random()*255);
+    let green = Math.floor(Math.random()*255);
+    let blue = Math.floor(Math.random()*255);
+    let color = `rgb(${red}, ${green} , ${blue})`;
+    return color;
+}
+
+btnn.addEventListener("click",function(){
+let h2= document.querySelector('h2');
+h2.innerText = getrandomcolor();
+h2.style.color = getrandomcolor();
+document.querySelector('div').style.backgroundColor= getrandomcolor();
+})
+
 
