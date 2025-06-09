0ed81016a85155a9e59be9ecb82446a1804074da
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-09 22:39:45 +0545

    extracting data from form

--- JS/Js p10/app.js
+++ JS/Js p10/app.js
@@ -80,10 +80,11 @@ let inp =document.querySelector("input");
 let form = document.querySelector("form");
 form.addEventListener("submit" , function(event){
 event.preventDefault();
-let user = document.querySelector("#user");
+let user =this.elements[0];
 let pass = document.querySelector("#pass");
 
 console.log(user.value);
 console.log(pass.value);
+alert( `hi ${user.value} your password is set to ${pass.value}`)
 })
  
