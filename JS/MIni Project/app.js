dad8c60ab93f859f19d101c87371985cc22afa9f
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-10 20:38:23 +0545

    to do app

--- JS/MIni Project/app.js
+++ JS/MIni Project/app.js
@@ -1,22 +1,40 @@
- let div= document.querySelector("div");
- let ul= document.querySelector("ul");
- let list= document.querySelectorAll("li");
+//  let div= document.querySelector("div");
+// //  let ul= document.querySelector("ul");
+//  let list= document.querySelectorAll("li");
 
- div.addEventListener("click",function(event){
-     event.stopPropagation();
-    console.log("div was clicked");
- })
- ul.addEventListener("click",function(event){
-     event.stopPropagation();
+//  div.addEventListener("click",function(event){
+//      event.stopPropagation();
+//     console.log("div was clicked");
+//  })
+//  ul.addEventListener("click",function(event){
+//      event.stopPropagation();
      
-    console.log("ul was clicked");
- })
+//     console.log("ul was clicked");
+//  })
 
- for(li of list){
- li.addEventListener("click",function(event){
-    event.stopPropagation();
-    console.log("li was clicked");
- });
-} 
+//  for(li of list){
+//  li.addEventListener("click",function(event){
+//     event.stopPropagation();
+//     console.log("li was clicked");
+//  });
+// } 
+
+
+//to do list
+let input = document.querySelector("input");
+let btn = document.querySelector("button");
+let ul= document.querySelector("ul");
+
+btn.addEventListener("click",function(){
+   let item = document.createElement("li");
+   console.log(input.value);
+   console.log("clicked");
+   item.innerText= input.value;
+   ul.append(item);
+   input.value="";
+
+
+})
+
 
  
