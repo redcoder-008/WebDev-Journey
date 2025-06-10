e364b596fe2a25820fdde90afd0265e6a42d2d99
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-10 18:37:13 +0545

    mini Project

---
+++ JS/MIni Project/app.js
@@ -0,0 +1,22 @@
+ let div= document.querySelector("div");
+ let ul= document.querySelector("ul");
+ let list= document.querySelectorAll("li");
+
+ div.addEventListener("click",function(event){
+     event.stopPropagation();
+    console.log("div was clicked");
+ })
+ ul.addEventListener("click",function(event){
+     event.stopPropagation();
+     
+    console.log("ul was clicked");
+ })
+
+ for(li of list){
+ li.addEventListener("click",function(event){
+    event.stopPropagation();
+    console.log("li was clicked");
+ });
+} 
+
+ 
