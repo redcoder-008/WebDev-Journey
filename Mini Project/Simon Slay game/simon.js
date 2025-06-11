e719fe1840e565393134885574c3f2df5c9ff918
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-11 13:06:20 +0545

    Simon Say Game

--- Mini Project/Simon Slay game/simon.js
+++ Mini Project/Simon Slay game/simon.js
@@ -6,11 +6,13 @@ let level = 0;
 let btn = document.querySelector("div");
 let h2 = document.querySelector("h2");
 let btns = ["yellow", "red", "purple", "green"];
+let highscore = 0;
+let h3 = document.querySelector("h3");
 
 document.addEventListener("keypress", function () {
     if (started == false) {
 
-        console.log("game started");
+        // console.log("game started");
         started = true;
         levelup();
     }
@@ -23,12 +25,14 @@ function btnflash(btn) {
     }, 250);
 
 
+
 }
 function userflash(btn) {
     btn.classList.add("userflash");
     setTimeout(function () {
         btn.classList.remove("userflash");
     }, 250);
+
 
 
 }
@@ -38,23 +42,63 @@ function levelup() {
     let randomidx = Math.floor(Math.random() * 3);
     let randomcolor = btns[randomidx];
     let randbtn = document.querySelector(`.${randomcolor}`);
-    console.log(randomcolor);
-    console.log(randbtn);
-
-
+    // console.log(randomcolor);
+    // console.log(randbtn);
     btnflash(randbtn);
+    gameseq.push(randomcolor);
+    userseq = [];
+    console.log(gameseq);
+    console.log(userseq);
 
 
+
+
+}
+function checkAns(idx) {
+
+    if (userseq[idx] == gameseq[idx]) {
+        // console.log("same val");
+        if (userseq.length == gameseq.length) {
+            setTimeout(levelup, 1000);
+
+
+        }
+    } else {
+        console.log("game over");
+        h2.innerHTML = `Game Over!  Your Score was ${level} <br>Press Any key to Restart`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function () {
+            document.querySelector("body").style.backgroundColor = "white";
+
+        }, 150);
+        if (highscore < level) {
+            highscore = level;
+            h3.innerText = `High Socre : ${highscore}`;
+        }
+        reset();
+    }
 }
 function btnpress() {
     let btn = this;
     userflash(btn);
-    console.log(btn);
+    // console.log(btn);
+    usercolor = btn.getAttribute("id");
+    userseq.push(usercolor);
+    checkAns(userseq.length - 1);
 
 
 }
 let allbtns = document.querySelectorAll(".btn");
 for (btn of allbtns) {
     btn.addEventListener("click", btnpress);
+}
+
+
+function reset() {
+    started = false;
+    gameseq = [];
+    userseq = [];
+    level = 0;
+
 }
 
