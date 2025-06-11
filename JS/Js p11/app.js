51468b18e3d7ad560f2169deed16f914cb6aedb6
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-11 20:36:23 +0545

    promises

--- JS/Js p11/app.js
+++ JS/Js p11/app.js
@@ -19,26 +19,48 @@
 
 
  //callback hell 
- h1= document.querySelector("h1");
+//  h1= document.querySelector("h1");
 
- setTimeout(() => {
-   
-     
-     
-}, 1000);
-function changecolor(color,delay,nextColorChange){
-    setTimeout(() => {
-        h1.style.color = color;
-        console.log("hello");
-        if(nextColorChange) nextColorChange();
+ 
+// function changecolor(color,delay,nextColorChange){
+//     setTimeout(() => {
+//         h1.style.color = color;
+//         console.log("hello");
+//         if(nextColorChange) nextColorChange();
         
-    }, delay);
+//     }, delay);
 
+//  }
+//  changecolor("red",1000,()=>{
+//     changecolor("green",1000,()=>{
+//         changecolor("yellow",1000,()=>{
+//             changecolor("aqua",1000);
+//         })
+//     })
+//  });
+
+ //  upar ka code me call back function likha huwa hai
+
+ //promises
+
+ function saveToDb(data,success,failure){
+    let internetSpeed = Math.floor(Math.random()*10 +1);
+    console.log(internetSpeed);
+
+    if(internetSpeed >= 5){
+        success();
+    }else{
+        failure();
+    }
  }
- changecolor("red",1000,()=>{
-    changecolor("green",1000,()=>{
-        changecolor("yellow",1000,()=>{
-            changecolor("aqua",1000);
-        })
+
+ saveToDb("karan",()=>{
+    console.log("Data saved successfully");
+    saveToDb("kumar",()=>{
+        console.log("another was was saved");
+    },()=>{
+        console.log("another data was not saved");
     })
- });
+ },()=>{
+    console.log("Data was not saved");
+ })
