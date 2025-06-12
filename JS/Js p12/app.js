24a78fa0a58a876c2b5f220aa58ea9ca1da8c4b3
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-12 22:06:06 +0545

    Await

--- JS/Js p12/app.js
+++ JS/Js p12/app.js
@@ -16,4 +16,26 @@ let demo= async()=>{
     return 5;
 }
 //await keyword
+console.log("Awake function")
+let h1= document.querySelector("h1");
+
+function changeColor(color,delay){
+    return new Promise ((resolve,reject)=>{
+        
+        setTimeout(()=>{
+            h1.style.color = color;
+            console.log(`Color Change to ${color}!`);
+            resolve ("Color change");
+            
+        },delay);
+    });
+}
+
+async function demoo(){
+   await changeColor("red",1000);
+  await  changeColor("yellow",1000);
+ await   changeColor("blue",1000);
+ await  changeColor("pink",1000);
+ changeColor("aqua",1000);
+}
   
