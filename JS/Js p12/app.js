6f42915fecf7e95a6f4961aa020670321263406b
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-12 22:38:45 +0545

    asyncs functions

--- JS/Js p12/app.js
+++ JS/Js p12/app.js
@@ -1,41 +1,41 @@
-  async function greet(){
+async function greet() {
     //   throw" some random error";
-//  console.log(a);
+    //  console.log(a);
     return "Hello";
-  }
+}
 
-  greet()
-  .then((res )=>{
-    console.log("Completed successfully",res)
-  })
-.catch((ree)=>{
-    console.log("Opps!! some error",ree);
-})
+greet()
+    .then((res) => {
+        console.log("Completed successfully", res)
+    })
+    .catch((ree) => {
+        console.log("Opps!! some error", ree);
+    })
 // arrow function
-let demo= async()=>{
+let demo = async () => {
     return 5;
 }
 //await keyword
 console.log("Awake function")
-let h1= document.querySelector("h1");
+let h1 = document.querySelector("h1");
 
-function changeColor(color,delay){
-    return new Promise ((resolve,reject)=>{
-        
-        setTimeout(()=>{
+function changeColor(color, delay) {
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
             h1.style.color = color;
             console.log(`Color Change to ${color}!`);
-            resolve ("Color change");
-            
-        },delay);
+            resolve("Color change");
+
+        }, delay);
     });
 }
 
-async function demoo(){
-   await changeColor("red",1000);
-  await  changeColor("yellow",1000);
- await   changeColor("blue",1000);
- await  changeColor("pink",1000);
- changeColor("aqua",1000);
+async function demoo() {
+    await changeColor("red", 1000);
+    await changeColor("yellow", 1000);
+    await changeColor("blue", 1000);
+    await changeColor("pink", 1000);
+    changeColor("aqua", 1000);
 }
-  
+
