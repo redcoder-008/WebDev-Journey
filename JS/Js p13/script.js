1cd5191ef6c1fa8a23b95bd42c7f6c2336d58964
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-15 21:06:27 +0545

    get random image

--- JS/Js p13/script.js
+++ JS/Js p13/script.js
@@ -1,26 +1,47 @@
-console.log("hello")
-let url = "https://catfact.ninja/fact";
+// console.log("hello")
+// let url = "https://catfact.ninja/fact";
+// btn = document.querySelector("Button");
+// p = document.querySelector(".result");
+
+// async function getFacts() {
+//     try {
+//         let res = await axios.get(url);
+//         console.log(res.data.fact);
+//         return res.data.fact;
+
+//     }
+//     catch (e) {
+//         console.log("Bc error aa gya", e);
+//     }
+// }
+
+// getFacts();
+// p.innerText = "karan";
+
+// btn.addEventListener("click", async () => {
+//     console.log("hello ");
+//     let fact = await getFacts();
+
+//     p.innerText = fact;
+// })
+
+let url2 = "https://dog.ceo/api/breeds/image/random";
 btn = document.querySelector("Button");
-p = document.querySelector(".result");
+img = document.querySelector("#result");
 
-async function getFacts(){
-    try {
-        let res = await axios.get(url);
-        console.log(res.data.fact);
-        return res.data.fact;
-     
-    } 
-    catch(e){
-        console.log("Bc error aa gya",e);
+async function getimage(){
+    try{
+        let res = await axios.get(url2);
+        return (res.data.message);
     }
+    catch(e){
+        console.log("Error aa gya - ",e);
+    }
+    
+
 }
-
-getFacts();
-p.innerText = "karan";
-
-btn.addEventListener("click",async()=>{
-    console.log("hello ");
-    let fact = await getFacts();
-  
-    p.innerText = fact;
+btn.addEventListener("click", async()=>{
+    let link = await getimage();
+    console.log(link);
+    img.setAttribute("src",link);
 })
