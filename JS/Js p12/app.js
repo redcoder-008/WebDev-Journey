a101455c66ed5af65856148d809b6366ec9d99d7
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-15 20:16:53 +0545

    First data was fetched

--- JS/Js p12/app.js
+++ JS/Js p12/app.js
@@ -46,8 +46,18 @@ h1.innerText=  validres.fact;
 
 let url = "https://catfact.ninja/fact";
 fetch(url).then((response)=>{
-    console.log(response);
-   console.log( response.json());
+   return  response.json();
+})
+.then((data)=>{
+    console.log("data 1: ",data.fact);
+    return fetch(url);
+
+})
+.then((res)=>{
+    return res.json();
+})
+.then((data2)=>{
+    console.log("Data 2 : " , data2.fact);
 })
 .catch((err)=>{
     console.log(err);
