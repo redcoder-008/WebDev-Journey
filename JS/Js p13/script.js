9d62ea8796fb88487301f96d22b22b46811b5851
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-15 20:55:24 +0545

    api with html

--- JS/Js p13/script.js
+++ JS/Js p13/script.js
@@ -1,14 +1,26 @@
 console.log("hello")
 let url = "https://catfact.ninja/fact";
+btn = document.querySelector("Button");
+p = document.querySelector(".result");
 
-async function getfacts(){
+async function getFacts(){
     try {
         let res = await axios.get(url);
         console.log(res.data.fact);
+        return res.data.fact;
      
-    }catch(e){
+    } 
+    catch(e){
         console.log("Bc error aa gya",e);
     }
 }
 
-getfacts();
+getFacts();
+p.innerText = "karan";
+
+btn.addEventListener("click",async()=>{
+    console.log("hello ");
+    let fact = await getFacts();
+  
+    p.innerText = fact;
+})
