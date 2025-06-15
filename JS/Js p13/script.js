a72cb1fd1f2005889a6b802fafaafff4effa3178
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-15 21:22:32 +0545

    generate new image

--- JS/Js p13/script.js
+++ JS/Js p13/script.js
@@ -29,19 +29,19 @@ let url2 = "https://dog.ceo/api/breeds/image/random";
 btn = document.querySelector("Button");
 img = document.querySelector("#result");
 
-async function getimage(){
-    try{
+async function getimage() {
+    try {
         let res = await axios.get(url2);
         return (res.data.message);
     }
-    catch(e){
-        console.log("Error aa gya - ",e);
+    catch (e) {
+        console.log("Error aa gya - ", e);
     }
-    
+
 
 }
-btn.addEventListener("click", async()=>{
+btn.addEventListener("click", async () => {
     let link = await getimage();
     console.log(link);
-    img.setAttribute("src",link);
+    img.setAttribute("src", link);
 })
