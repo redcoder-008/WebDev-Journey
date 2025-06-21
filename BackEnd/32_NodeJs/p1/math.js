ac7f701e5b79316d40c2c248f3c6e0f34c223243
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-21 12:16:02 +0545

    require and export

---
+++ BackEnd/32_NodeJs/p1/math.js
@@ -0,0 +1,11 @@
+const sum =(a,b)=> a+b;
+const mul =(a,b)=> a*b;
+const pi= 3.14;
+const g= 9.8;
+let obj ={
+    sum:sum,
+    mul:mul,
+    pi:pi,
+    g:g,
+}
+module.exports= obj;
--- BackEnd/32_NodeJs/p1/script.js
+++ BackEnd/32_NodeJs/p1/script.js
@@ -1,7 +1,10 @@
-//  let n=5;
- let args = process.argv;
- for(let i=2;i<args.length;i++){
+// //  let n=5;
+//  let args = process.argv;
+// //  for(let i=2;i<args.length;i++){
     
 
-     console.log("Welcome to my world dear ",args[i]);
- }
+// //      console.log("Welcome to my world dear ",args[i]);
+// //  }
+ const math = require("./math");
+ console.log(math.pi);
+ 
