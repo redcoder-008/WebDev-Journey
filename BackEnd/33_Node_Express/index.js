16f313d25a7b07103ea7af9771a06394c1f0dc8d
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-24 21:18:25 +0545

    use on express

--- BackEnd/33_Node_Express/index.js
+++ BackEnd/33_Node_Express/index.js
@@ -7,3 +7,10 @@ let port = 3000;
 app.listen(port,()=>{
     console.log(`app listening on port ${port}`)
 });
+
+app.use((req,res)=>{
+    console.log("New Request received");
+    // res.send("This is basic response");
+    let code = "<h1>Karan Kumar Kamat</h1>"
+    res.send(code);
+});
