8a830f95177b6138c14c074fe26d5a0395afef8c
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-24 21:29:46 +0545

    routing

--- BackEnd/33_Node_Express/index.js
+++ BackEnd/33_Node_Express/index.js
@@ -2,15 +2,25 @@ const express = require("express");
 const app = express();
 console.dir(app);
 
-let port = 3000;
+let port = 8080;
 
 app.listen(port,()=>{
     console.log(`app listening on port ${port}`)
 });
 
-app.use((req,res)=>{
-    console.log("New Request received");
-    // res.send("This is basic response");
-    let code = "<h1>Karan Kumar Kamat</h1>"
-    res.send(code);
+// app.use((req,res)=>{
+//     console.log("New Request received");
+//     // res.send("This is basic response");
+//     let code = "<h1>Karan Kumar Kamat</h1>"
+//     // res.send(code);
+// });
+
+app.get("/",(req,res)=>{
+    res.send("you choosed root path");
+});
+app.get("/apple",(req,res)=>{
+    res.send("apple is red in color");
+});
+app.get("/banana",(req,res)=>{
+    res.send("banana is yellow in color");
 });
