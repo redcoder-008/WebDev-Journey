35861a95790d3b0186635fd38b678d087abbb82d
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-27 22:42:14 +0545

    Using EJS

--- BackEnd/34_Node_EJS/EJSdir/index.js
+++ BackEnd/34_Node_EJS/EJSdir/index.js
@@ -5,3 +5,9 @@ const app = express();
  app.listen(port,()=>{
     console.log(`listening on port ${port}`);
  })
+
+ app.set("view engine","ejs");
+ app.get("/",(req,res)=>{
+    res.render("home.ejs");
+
+ })
