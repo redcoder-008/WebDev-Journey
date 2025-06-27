d59136903270acc3ab6dd32750b4b53745fa67d8
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-06-27 22:48:07 +0545

    Views Directory

--- BackEnd/34_Node_EJS/EJSdir/index.js
+++ BackEnd/34_Node_EJS/EJSdir/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
  const port = 8080;
+ const path = require("path");
 
  app.listen(port,()=>{
     console.log(`listening on port ${port}`);
  })
 
  app.set("view engine","ejs");
+app.set("views", path.join(__dirname,"/views")); //can run file from parent folder
+
  app.get("/",(req,res)=>{
     res.render("home.ejs");
 
+ });
+ app.get("/hello",(req,res)=>{
+    res.send("hellow");
+
  })
+
