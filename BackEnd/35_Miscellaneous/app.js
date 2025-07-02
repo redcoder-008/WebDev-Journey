8aeb3f2f72116be370be77f2606f7a30166ee073
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-07-02 21:40:16 +0545

    new  Operator

--- BackEnd/35_Miscellaneous/app.js
+++ BackEnd/35_Miscellaneous/app.js
@@ -1,12 +1,22 @@
 
-//factory function
-function PersonMaker(name,age){
-    const person ={
-        name: name,
-        age:age,
-        talk(){
-            console.log(`Hellow i am ${this.name}`);
-        },
-    };
-    return person;
+// //factory function
+// function PersonMaker(name,age){
+//     const person ={
+//         name: name,
+//         age:age,
+//         talk(){
+//             console.log(`Hellow i am ${this.name}`);
+//         },
+//     };
+//     return person;
+// }
+
+function Person(name,age){
+    this.name= name;
+    this.age = age;
+    
 }
+Person.prototype.talk= function (){
+    console.log(`Hi, i am ${this.name}`);
+};
+let p1 = new Person("karan",20);
