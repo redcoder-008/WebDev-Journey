df7aeff40c8339ca9cbad70c0a2eb6fa495e454d
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-07-02 22:30:41 +0545

    Get,post request, revisiting js 00-s, object prototype,factory function , class and Inheritance in js

--- BackEnd/35_Miscellaneous/app.js
+++ BackEnd/35_Miscellaneous/app.js
@@ -10,7 +10,9 @@
 //     };
 //     return person;
 // }
+ 
 
+//new Operator
 function Person(name,age){
     this.name= name;
     this.age = age;
@@ -20,3 +22,43 @@ Person.prototype.talk= function (){
     console.log(`Hi, i am ${this.name}`);
 };
 let p1 = new Person("karan",20);
+
+class Aadmi{
+    constructor(name,age){
+        this.name= name;
+        this.age = age;
+
+    }
+    talk(){
+        console.log(`I am $`)
+    }
+}
+class Student extends Aadmi{
+    constructor(name,age,subject){
+        super(name,age); //parent class constructor is called
+
+        this.subject= subject;
+
+    }
+}
+let t1 = new Student("karan" ,20,"DSA");
+
+class Mammal{
+    constructor (name){
+        this.name = name;
+    }
+    eat(){
+        console.log(`${this.name} can eat`);
+    }
+}
+class Dog extends Mammal{
+    constructor (name){
+        super(name);
+    }
+    bark(){
+        console.log(`${this.name} can bark`);
+    }
+    
+}
+
+let d1= new Dog("Tommy");
