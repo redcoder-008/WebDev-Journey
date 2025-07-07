65b79fe365a607d632cc0d9cefc6fe98c969e227
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-07-07 23:32:06 +0545

    Rest ful api

--- BackEnd/36(REST)/index.js
+++ BackEnd/36(REST)/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port =8080;
+app.set("views", path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+app.set("view engine","ejs");
+app.set(express.static(path.join(__dirname,"public")));
 app.listen(port, ()=>{
-    console,log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 })
-app.get
+// app.set("public",path.join(__dirname,"public")); 
+
+app.get("/",(req,res)=>{
+    res.send("Port is working");
+})
