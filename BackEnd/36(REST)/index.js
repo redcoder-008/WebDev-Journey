01bda3093223aa84d9a1a3a93a13bf7f72e6eac7
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-07-09 21:03:57 +0545

    created array

--- BackEnd/36(REST)/index.js
+++ BackEnd/36(REST)/index.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port =8080;
+let posts = [
+
+    {
+        username:"redcoder",
+        content: "I am 5star coder"
+    },
+    {
+        username:"Karan",
+        content:"I am a senior software engineer"
+    }
+];
 app.set("views", path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
@@ -15,3 +26,4 @@ app.get("/",(req,res)=>{
     res.send("Port is working");
 })
 // rest api
+
