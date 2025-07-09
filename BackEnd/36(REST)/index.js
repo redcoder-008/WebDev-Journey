ec1abf89c52ea879e3a1252bb0be5d5b425b910b
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-07-09 21:25:01 +0545

    Made Index Route

--- BackEnd/36(REST)/index.js
+++ BackEnd/36(REST)/index.js
@@ -16,14 +16,16 @@ let posts = [
 app.set("views", path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
-app.set(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"public")));
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`);
 })
 // app.set("public",path.join(__dirname,"public")); 
 
-app.get("/",(req,res)=>{
-    res.send("Port is working");
+app.get("/posts",(req,res)=>{
+    // res.send("Port is working");
+    res.render("index.ejs",{posts});
+
 })
 // rest api
 
