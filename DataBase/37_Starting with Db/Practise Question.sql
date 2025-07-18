72b42a82b8c258098f08d1f917c6e08bbf5bac97
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-07-18 11:11:55 +0545

    SQL part 2 completed

--- DataBase/37_Starting with Db/Practise Question.sql
+++ DataBase/37_Starting with Db/Practise Question.sql
@@ -21,3 +21,51 @@ set ctc = ctc+ctc*(0.25);
 alter table teacher add column city varchar(30) default"gurgaon";
 
 alter table teacher drop column ctc;
+use  assignment;
+create table students(
+rollno int,
+name varchar(20),
+city varchar(20),
+marks int);
+insert into students (rollno,name,city,marks)
+values(110,"adam","Delhi",76),
+(108,"bob","Mumbai",65),
+(124,"casey","Pune",94),
+(112,"Duke","Pune",80);
+
+select *from students where marks>75;
+select distinct city from students;
+select city 
+from students 
+group by city;
+
+select city ,max(marks)
+from students 
+group by city;
+select avg(marks)
+from students;
+alter table students 
+add column grade varchar(2);
+set sql_safe_updates =0;
+update students
+set grade ='0'
+where marks >=80;
+update students 
+set grade = 'A'
+where marks >=70 and marks <80;
+update students 
+set grade ='B'
+where marks<=70 and marks >60;
+select *from students;
+
+
+
+
+
+
+
+
+
+
+
+
