d0881ef9aa81eed54d1a5dd1d27747dec6d5333d
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-07-20 21:17:24 +0545

    Setting up route

--- DataBase/39_NodeWithSQL/index.js
+++ DataBase/39_NodeWithSQL/index.js
@@ -1,5 +1,7 @@
  const { faker } = require('@faker-js/faker');
  const mysql = require('mysql2');
+ const express = require("express");
+ const app = express();
  const connection = mysql.createConnection({
     host:'localhost',
     user:'root',
@@ -17,13 +19,13 @@
    
   ];
 }
-let data=[];
-for(let i=0;i<100;i++){
-  data.push(getRandomUser());
+// let data=[];
+// for(let i=0;i<100;i++){
+//   data.push(getRandomUser());
 
-}
+// }
 
-
+/*
 
 let user = [ ["123c","Thirduser","Third@gmail.com","abc3" ],["123e","thirduser","third@gmail.com","abc4"]];
  let q ="INSERT INTO user (id,username,email,password) VALUES ?";
@@ -37,7 +39,28 @@ let user = [ ["123c","Thirduser","Third@gmail.com","abc3" ],["123e","thirduser",
         console.log(err);
     }
     connection.end();
+    */
+   app.listen("8080",()=>{
+     console.log("server is running");
+    });
+    
+    app.get("/",(req,res)=>{
+      // res.send("welcome to home page");
+      let q= 'select count(*) from user';
+      try{
+        connection.query(q,(err,result)=>{
+          if(err) throw err;
+          console.log(result[0] ['count(*)']);
+          res.send('success');
+        });
+      }catch(err){
+        console.log(err);
+        res.send("Some error in DB");
+      }
+      
+   
 
+    });
 
 
 
