c4ccea513f4e9511b4d6fa9e7f95e38e603b928c
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-07-20 21:40:38 +0545

    user route set

--- DataBase/39_NodeWithSQL/index.js
+++ DataBase/39_NodeWithSQL/index.js
@@ -1,22 +1,25 @@
- const { faker } = require('@faker-js/faker');
- const mysql = require('mysql2');
- const express = require("express");
- const app = express();
- const connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    database:'karandatabase',
-    password:'karan@mysql',
-    port:3307
- });
-  let  getRandomUser=()=>{
+const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+const express = require("express");
+const app = express();
+const path = require("path");
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'karandatabase',
+  password: 'karan@mysql',
+  port: 3307
+});
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+let getRandomUser = () => {
   return [
-     faker.string.uuid(),
-     faker.internet.username(), // before version 9.1.0, use userName()
-     faker.internet.email(),
-  
-     faker.internet.password(),
-   
+    faker.string.uuid(),
+    faker.internet.username(), // before version 9.1.0, use userName()
+    faker.internet.email(),
+
+    faker.internet.password(),
+
   ];
 }
 // let data=[];
@@ -40,27 +43,44 @@ let user = [ ["123c","Thirduser","Third@gmail.com","abc3" ],["123e","thirduser",
     }
     connection.end();
     */
-   app.listen("8080",()=>{
-     console.log("server is running");
-    });
-    
-    app.get("/",(req,res)=>{
-      // res.send("welcome to home page");
-      let q= 'select count(*) from user';
-      try{
-        connection.query(q,(err,result)=>{
-          if(err) throw err;
-          console.log(result[0] ['count(*)']);
-          res.send('success');
-        });
-      }catch(err){
-        console.log(err);
-        res.send("Some error in DB");
-      }
-      
-   
+app.listen("8080", () => {
+  console.log("server is running");
+});
 
+app.get("/", (req, res) => {
+  // res.send("welcome to home page");
+  let q = 'select count(*) from user';
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let count = result[0]['count(*)'];
+      res.render("home.ejs", { count });
     });
+  } catch (err) {
+    console.log(err);
+    res.send("Some error in DB");
+  }
+});
+4
+app.get("/user",(req,res)=>{
+  let q= 'SELECT *from user';
+  try{
+    connection.query(q,(err,result)=>{
+      if(err) throw err;
+      res.send("data printed");
+      console.log(result);
+    })
+  }catch(err){
+    console.log(err);
+    res.send("some mistake");
+  }
+})
+
+
+
+
+
+
 
 
 
