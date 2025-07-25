a5dcea6265fa478c25515caaaf4ff22d0000912c
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-07-25 14:35:20 +0545

    mongoose basic

--- DataBase/42_MongoDB(P2)/index.js
+++ DataBase/42_MongoDB(P2)/index.js
@@ -1,2 +1,13 @@
  const mongoose = require("mongoose");
- mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+main().then((res)=>{
+    // console.log(res);
+    console.log("Successful");
+});
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
+//mongoose 
