94794a285d1aa8a5cae2661b5113a7f52f04fcbc
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-08-01 21:06:32 +0545

    Schema Defination

--- DataBase/42_MongoDB(P2)/index.js
+++ DataBase/42_MongoDB(P2)/index.js
@@ -10,4 +10,10 @@ main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
 }
-//mongoose 
+
+
+const userSchema = new mongoose.Schema({
+  name:String,
+  email:String,
+  age:Number,
+});
