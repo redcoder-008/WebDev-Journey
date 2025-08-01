d0e5f5790dd56263ef33cce31986bd4f3a7442c7
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-08-01 22:06:43 +0545

    Insertion of many data

--- DataBase/42_MongoDB(P2)/index.js
+++ DataBase/42_MongoDB(P2)/index.js
@@ -24,5 +24,18 @@ const employee = mongoose.model("employe" ,userSchema);
 const user1 = new User({name:"Karan",email:"Karan@gmail.com",age:20});
 const user2 = new User({name:"Kumar",email:"Kumar@gmail.com",age:20});
 user1.save();
-user2.save();
+user2.save().then((res)=>{
+  console.log("Saved");
+}).catch((err)=>{
+  console.log(err);
 
+});
+ User.insertMany([
+  {name:"kum",email:"sample@gmaail.com",age:21},
+  { name:"Kumar",email:"Kumar@gmail.com",age:30},
+  {name:"Kamat",email:"Kamat@gmail.com",age:23}
+ ]).then((res)=>{
+  console.log(res);
+ }).catch((err)=>{
+  console.log(err);
+ });
