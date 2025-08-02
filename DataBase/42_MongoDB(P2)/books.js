cddec9fbc1e19af1297dfe96829b84ad01f3108e
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-08-02 13:11:01 +0545

    Schema type validation

--- DataBase/42_MongoDB(P2)/books.js
+++ DataBase/42_MongoDB(P2)/books.js
@@ -1,38 +1,78 @@
- const mongoose = require("mongoose");
- main().then(()=>{
+const mongoose = require("mongoose");
+main().then(() => {
     console.log("connection success")
- }).catch((err)=>{
+}).catch((err) => {
     console.log(err);
- });
- async function main(){
+});
+async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/amazon");
-    
- }
 
- const bookSchema = mongoose.Schema({
-    title:{
-        type:String,
-        required:true,
+}
+
+const bookSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
     },
-    author:{
-        type:String,
+    author: {
+        type: String,
 
     },
-    price:{
-        type:Number,
+    price: {
+        type: Number,
     }
- });
- const Book = mongoose.model("Book",bookSchema);
+});
+const Book = mongoose.model("Book", bookSchema);
 
- let book1 = new Book({
+let book1 = new Book({
     // title:"DSA",
-    author:"KAran",
-    price:242,
- })
+    author: "KAran",
+    price: 242,
+})
 
- book1.save().then((res)=>{
+book1.save().then((res) => {
     console.log(res);
- }).catch((err)=>{
+}).catch((err) => {
     console.log("Some mistake");
     console.log(err);
- });
+});
+
+const kitabSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        maxLength:20,//name should not be more than 20
+    },
+    author: {
+        type: String,
+    },
+    price: {
+        type: Number,
+        min:1,
+    },
+    discount: {
+        type: Number,
+        default: 0,
+
+    },
+    category:{
+        type:String,
+        enum:["fiction","non-fiction"]//category inhi mese hona chaiye
+    }
+});
+let kitab = mongoose.model("kitab", kitabSchema);
+
+let kitab1 = new kitab({
+    title: "MArvel comic",
+    author: "xyz",
+    price: 10,
+    category:"fiction",
+});
+
+kitab1.save().then((res) => {
+    console.log("Data Inserted");
+}).catch((err) => {
+
+    console.log(err);
+    console.log("Kuch badbad hai ");
+});
