8f2ad0ea6bdc95ad4278491cbdb13069e3890768
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-08-02 12:35:04 +0545

    Schema Validation

--- DataBase/42_MongoDB(P2)/books.js
+++ DataBase/42_MongoDB(P2)/books.js
@@ -5,17 +5,34 @@
     console.log(err);
  });
  async function main(){
-    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    await mongoose.connect("mongodb://127.0.0.1:27017/amazon");
     
  }
 
  const bookSchema = mongoose.Schema({
     title:{
         type:String,
-        
+        required:true,
     },
     author:{
         type:String,
 
+    },
+    price:{
+        type:Number,
     }
+ });
+ const Book = mongoose.model("Book",bookSchema);
+
+ let book1 = new Book({
+    // title:"DSA",
+    author:"KAran",
+    price:242,
  })
+
+ book1.save().then((res)=>{
+    console.log(res);
+ }).catch((err)=>{
+    console.log("Some mistake");
+    console.log(err);
+ });
