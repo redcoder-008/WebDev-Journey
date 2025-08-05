5b14daf153164d606bda2c80774a0ff1b97d08d7
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-08-05 23:09:05 +0545

    database testing completed

--- DataBase/43_MongoDBwithExpress/index.js
+++ DataBase/43_MongoDBwithExpress/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const Chat = require("./models/chat.js");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -23,3 +24,15 @@ app.get("/", (req, res) => {
     res.send("Root is working")
 })
 
+let chat1 = new Chat({
+    from:"karan",
+    to:"Kumar",
+    message:"I am testing",
+    createdAt:new Date()
+})
+
+chat1.save().then((res)=>{
+    console.log(res);
+}).catch((err)=>{
+    console.log("kuch mmistake hai ");
+})
