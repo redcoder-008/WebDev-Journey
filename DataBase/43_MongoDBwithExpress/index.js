3955c967c34f7fdaa7c8d3e952afad559c507ffc
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-08-05 22:50:37 +0545

    setting up database and views

--- DataBase/43_MongoDBwithExpress/index.js
+++ DataBase/43_MongoDBwithExpress/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path");
 
-app.listen(8080,()=>{
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+
+main().then(() => {
+    console.log("Connection successful");
+}).catch(err => console.log(err));
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+
+app.listen(8080, () => {
     console.log("Listening")
 });
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Root is working")
 })
 
