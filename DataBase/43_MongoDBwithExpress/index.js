193bbbe1099d6f094b88dbd65c84176a73b84af4
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-08-06 13:07:15 +0545

    update route

--- DataBase/43_MongoDBwithExpress/index.js
+++ DataBase/43_MongoDBwithExpress/index.js
@@ -3,12 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override")
 
 app.set("views", path.join(__dirname, "views"));//to connect ejs file
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));//it shows that from where css,js file is served
 app.use(express.urlencoded({ extended: true })); //to parese data ,get from form
-
+app.use(methodOverride("_method"));
 
 main().then(() => {
     console.log("Connection successful");
@@ -58,4 +59,26 @@ app.post("/chats", (req, res) => {
     res.redirect("/chats")
 
 
+})
+
+//edit route
+app.get("/chats/:id/edit", async (req,res)=>{
+    let {id} = req.params;
+    let chat =await Chat.findById(id);
+
+
+    res.render("edit.ejs",{chat});
+})
+//update route
+app.put("/chats/:id", async (req,res)=>{
+    let {id} = req.params;
+    let {message:newMessage}=req.body;
+    let updatedChat =await Chat.findByIdAndUpdate(
+        id,
+        {message:newMessage} ,
+        {runValidators:true , new:true},
+
+    );
+    console.log(updatedChat);
+    res.redirect("/chats");
 })
