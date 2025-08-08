94ed527bb57de4001bb4de74aa4e6d48b9e95c27
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-08-08 11:30:37 +0545

    boilerplate successfully tested

--- Projects/Airbnb/app.js
+++ Projects/Airbnb/app.js
@@ -12,6 +12,7 @@ app.set("view engine ", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 
 app.listen(8080, () => {
