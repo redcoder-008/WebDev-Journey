88928325e97c4ea191768056cd4244694c83a72d
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-09-12 20:20:03 +0545

    Middleware validation

--- BackEnd/46_MiddleWare/app.js
+++ BackEnd/46_MiddleWare/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log("Listening on port 8080");
 });
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("hello world")
 });
 
@@ -13,14 +13,40 @@ app.get("/",(req,res)=>{
 // app.use(()=>{
 //     console.log("YOur first middleware")
 // });
-app.use((req,res)=>{
-     let {query} = req.query;
-    console.log(query);
-    console.log("Middleware");
-    res.send("OK");
+// app.use((req,res,next)=>{
+//      let {query} = req.query;
+//     // console.log(query);
+//     // console.log("Middleware");
+//     res.send("OK");
+//     next();
+// })
+//ready to go 
+
+// app.use((req,res,next)=>{
+//     req.date = new Date(Date.now()).toString(); //it creates current date
+//     console.log(req.method,req.hostname,req.path,req.date);
+//     next();
+
+// });
+
+app.use("/api", (req, res, next) => {
+    let { token } = req.query;
+    if (token == "giveaccess") {
+
+        res.send("Data");
+        console.log("Data sent");
+    } else {
+        res.send("Invalid");
+    }
+    next();
 })
-app.get("/random",(req,res)=>{
-   
+
+app.use("/random", (req, res, next) => {
+    console.log("I am only for random");
+    next();
+})
+
+app.get("/random", (req, res) => {
+
     res.send("Random page");
 })
-//ready to go 
