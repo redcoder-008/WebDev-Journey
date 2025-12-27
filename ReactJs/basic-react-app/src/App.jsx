ae4375861729f4d9558b810b2e79f2636bc5db75
Karan Kumar Kamat <164160892+redcoder-008@users.noreply.github.com>
2025-12-27 19:44:41 +0545

    braces

--- ReactJs/basic-react-app/src/App.jsx
+++ ReactJs/basic-react-app/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import Title from "./Title.jsx";
 
 import Test2 from "./Test.jsx";
+let name = "karan";
+
 function Description() {
   return <h3>I am description</h3>;
 }
@@ -11,6 +13,7 @@ function App() {
   return (
     <div>
       <button>Click me </button>
+      <h1>Hi,{name}</h1>
       <Title />
       <Test2 />
       <Description />
